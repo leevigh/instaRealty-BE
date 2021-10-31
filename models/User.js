1df8bcda8ed9c75dde9a47f1477a3ba30f25986b
leevigh <francisking151@gmail.com>
2021-10-31 20:14:14 +0100

    Added email validator to User model

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+// const bcrypt = require('bcryptjs')
+// const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -14,6 +14,11 @@ const userSchema = mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
+        validate: value => {
+            if(!validator.isEmail(value)) {
+                throw new Error({error: 'Invalid Email address'})
+            }
+        }
     },
     password: {
         type: String,
@@ -24,38 +29,8 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         default: "regular"
-    },
-    // tokens: [{
-    //     token: {
-    //         type: String,
-    //         required: true
-    //     }
-    // }]
+    }
 });
-
-// userSchema.pre('save', async function(next) {
-//     //Hash the password before saving the user model
-//     const user = this;
-//     if(user.isModified('password')) {
-//         user.password = await bcrypt.hash(user.password, 10)
-//     }
-//     next()
-// })
-
-// userSchema.methods.generateAuthToken = async function() {
-//     //Generate user auth token
-//     const user = this;
-//     const token = jwt.sign({
-//         email: user.email,
-//         id: user._id,
-//         role: user.role
-//         }, 
-//         process.env.JWT_KEY)
-
-//     user.tokens = user.tokens.concat({token})
-//     await user.save()
-//     return token
-// }
 
 const User = mongoose.model('User', userSchema)
 module.exports = User
